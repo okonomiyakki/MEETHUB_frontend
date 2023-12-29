9320c4b96741e9a038d9689aa50c1643a4ed7bb3
okonomiyakki <parkji12532@naver.com>
2023-12-30 07:13:46 +0900

    feat: home 페이지에 네비게이션바 컴포넌트 연결

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,11 @@
+import NavBar from '../../components/navBar/navBar'
 import Homebody from "../../components/homeBody/homeBody";
 import styles from './home.module.scss'
 
 export function Home() {
   return (
     <div id={styles.homePage}>
+      <NavBar />
       <Homebody />
     </div>
   )
