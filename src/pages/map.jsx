3aad3a83630bb461b48bf70f3f304dc23231cd8c
okonomiyakki <parkji12532@naver.com>
2023-12-30 05:43:03 +0900

    faet: map 페이지 연결

---
+++ src/pages/map.jsx
@@ -0,0 +1,11 @@
+import SearchPlace from '../components/map/serchplace/searchPlace'
+
+export function Map() {
+  return (
+    <div id="MapPage">
+      <SearchPlace />
+    </div>
+  )
+}
+
+export default Map;
