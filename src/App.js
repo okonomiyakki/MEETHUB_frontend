56a2f49db28f34a1033e62323761ef78715c9aa9
okonomiyakki <parkji12532@naver.com>
2023-12-30 05:43:25 +0900

    feat: Home, map 페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import './App.css';
+import './App.scss';
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/home/home';
+import { Map } from './pages/map';
 
 function App() {
     function setScreenSize() {
@@ -12,7 +14,10 @@ function App() {
     });
     return (
         <div>
-            <Routes></Routes>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/map" element={<Map />}></Route>
+            </Routes>
         </div>
     );
 }
