eba38462571708ac3ba19689fac51691ea95e3a3
okonomiyakki <parkji12532@naver.com>
2023-12-30 07:16:59 +0900

    feat: 카카오 맵 연동

---
+++ src/pages/map/map.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../../components/navBar/navBar'
+import SearchPlace from '../../components/map/serchplace/searchPlace'
+
+export function Map() {
+  return (
+    <div id="MapPage">
+      <NavBar />
+      <SearchPlace />
+    </div>
+  )
+}
+
+export default Map;
