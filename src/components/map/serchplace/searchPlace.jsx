71af49653fad5ae5e466e910f890a6d828a19dd2
okonomiyakki <parkji12532@naver.com>
2023-12-30 05:40:13 +0900

    Feat: searchPlace 컴포넌트 구현(임시)

---
+++ src/components/map/serchplace/searchPlace.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+
+export function SearhPlace() {
+
+  return (
+    <div>
+      <Link to="/">
+        <span>처음으로 </span>
+      </Link>
+    </div>
+  )
+}
+
+export default SearhPlace
