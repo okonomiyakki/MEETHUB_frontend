01b1d51b09a9025d6b069d654132510c0eb1ced0
okonomiyakki <parkji12532@naver.com>
2023-12-31 02:13:37 +0900

    feat: 출발지 추가 기능 구현

--- src/components/map/landingMap/landingMap.jsx
+++ src/components/map/landingMap/landingMap.jsx
@@ -11,6 +11,11 @@ export function LandingMap({ searchPlace, lat, lng, name }) {
   let [Name, setName] = useState([]);               // 입력된 출발지 이름
   let [addName, setAddName] = useState([]);         // 추가된 출발지 이름
   let [hotPlace, setHotPlace] = useState([]);       // 추천 매장
+  let mmm = [];           // 소요시간 전체
+  let www = [];           // 가중치 추가된
+  let ggg = [];           // 가중치 값
+  let avgTime = [];       // 최소 소요시간
+  let endStation = [];    // 도착역
 
   // useEffect(() => {
   //   const container = document.getElementById('map'),
@@ -90,12 +95,73 @@ export function LandingMap({ searchPlace, lat, lng, name }) {
 
   }, [searchPlace, lat, lng, name])
 
+  const buttonAdd = (intext) => {
+    if ((location.includes(intext) & !addLoc.includes(intext)) && intext != "") {
+      let new_addLoc = [...addLoc];
+      new_addLoc.unshift(intext);
+      setAddLoc(new_addLoc);
+      // console.log("추가한 주소명", new_addLoc);
+
+      let new_addLatlng = [...addLatlng];
+      new_addLatlng.unshift(latlng[0]);
+      setAddLatlng(new_addLatlng);
+      // console.log("추가한 좌표값", new_addLatlng);
+
+      let new_addName = [...addName];
+      new_addName.unshift(name);
+      setAddName(new_addName);
+      // console.log("추가한 장소명", new_addName);
+
+      // let new_index = [...index];
+      // new_index.unshift(new_addLoc.length - new_addLoc.indexOf(intext));
+      // setIndex(new_index);
+      // console.log("추가한 인덱스값", new_index);
+    }
+    else {
+      alert('장소 클릭 후 눌러주세요.');
+      return;
+    }
+  }
+
+
+
   return (
     <>
       <div id="map" style={{ width: "1900px", height: "845px", position: 'relative', overflow: 'hidden' }}>
+      </div>
+
+      <div id={styles.addWrap}>
+        <div id={styles.addBox} className={styles.bg_white}>
+          <div className={styles.addOption}>
+            {/* <button type="button" onClick={() => reload()}>다시하기</button> */}
+            {/* <button id={styles.addBtn} className={styles.addBtn} onClick={() => start(addLatlng, addName)}>중간 장소 찾기</button> */}
+            <button className={styles.addBtn} onClick={() => buttonAdd(searchPlace)}>출발지 추가</button>
+            <div>{addName.map((a, i) => (
+              <div key={i} className={styles.submitAddress}>
+                <div>{a}</div>
+                <button className={styles.deleteBtn} onClick={() => {
+                  let copy = [...addLoc];
+                  let copy2 = [...addLatlng];
+                  let copy3 = [...addName];
+                  copy.splice(i, 1)
+                  copy2.splice(i, 1)
+                  copy3.splice(i, 1)
+                  setAddLoc(copy);
+                  setAddLatlng(copy2)
+                  setAddName(copy3);
+                }}>X</button>
+              </div>))}
+            </div>
+          </div>
+          <hr></hr>
+        </div>
+        {/* <div id={styles.stationBox} className={styles.bg_white}>
+        </div> */}
       </div>
     </>
   )
 }
 
 export default LandingMap
+
+
